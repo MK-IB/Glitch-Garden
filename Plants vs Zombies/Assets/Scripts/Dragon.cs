5bf927f7093dcd473f7f242154aa80aef0b80100
Mallik Ebadat <31974569+MK-IB@users.noreply.github.com>
2020-06-03 22:49:03 +0530

    More Attackers Added

---
+++ Plants vs Zombies/Assets/Scripts/Dragon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+   [SerializeField] GameObject fireball, mouth;
+   GameObject fireballParent;
+   const string FIREBALL_PARENT_NAME = "Fireballs";
+
+   void Start()
+   {
+      CreateFireballParent();
+   }
+
+   private void CreateFireballParent()
+   {
+      fireballParent = GameObject.Find(FIREBALL_PARENT_NAME);
+
+      if(!fireballParent)
+      {
+         fireballParent = new GameObject(FIREBALL_PARENT_NAME);
+      }
+   }
+
+   public void ThrowFireball()
+   {
+      GameObject fireballs =
+      Instantiate(fireball, mouth.transform.position, mouth.transform.rotation) as GameObject;
+      fireballs.transform.parent = fireballParent.transform;
+   }
+}
