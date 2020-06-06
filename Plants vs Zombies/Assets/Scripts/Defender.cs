a0c3ffb0687557ad6c63de861f786e94e959de46
Mallik Ebadat <31974569+MK-IB@users.noreply.github.com>
2020-06-06 11:48:40 +0530

    Upto level 10

--- Plants vs Zombies/Assets/Scripts/Defender.cs
+++ Plants vs Zombies/Assets/Scripts/Defender.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Defender : MonoBehaviour
 {
     [SerializeField] int starCost = 100;
-    
+    float waitTime;
+    void Start()
+    {
+        waitTime = Random.Range(30f, 45f);
+        StartCoroutine(DestroyDefender());
+    }
+
+    IEnumerator DestroyDefender()
+    {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+
+    }
     public int GetStarCost()
     {
         return starCost;
