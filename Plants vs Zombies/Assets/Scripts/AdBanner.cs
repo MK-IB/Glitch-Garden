1f71d48665f2ed1987b2f7e29a727093f510e41e
Mallik Ebadat <31974569+MK-IB@users.noreply.github.com>
2020-06-12 11:54:17 +0530

    Ads Added

---
+++ Plants vs Zombies/Assets/Scripts/AdBanner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdBanner : MonoBehaviour {
+
+    public string gameId = "3628723";
+    public string placementId = "Options";
+    public bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while(!Advertisement.IsReady (placementId))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Show (placementId);
+    }
+}
